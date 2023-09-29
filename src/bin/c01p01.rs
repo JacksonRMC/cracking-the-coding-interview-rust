203a6ccebb35cd1f9ec4298b66df772a2fbd29df
JacksonRMC <jcarter0803@gmail.com:wq>
2023-09-29 12:40:39 -0700

    Inital attempt rust Cracking codeing interview

--- src/bin/c01p01.rs
+++ src/bin/c01p01.rs
@@ -1,32 +1,8 @@
 fn all_chars_unique_part_a(s: &str) -> bool {
-    use std::collections::HashSet;
-    let mut characters: HashSet<char> = HashSet::new();
-
-    for c in s.chars() {
-        if characters.contains(&c) {
-            return false;
-        }
-        characters.insert(c);
-    }
     true
 }
 
 fn all_chars_unique_part_b(s: &str) -> bool {
-    let mut bitfield: i64 = 0;
-    let a_int_char: i16 = 'a' as i16;
-
-    for c in s.chars() {
-        let mut int_char: i16 = c as i16;
-        int_char -= a_int_char;
-
-        if (1 << int_char) & bitfield != 0 {
-            return false;
-        }
-
-        // set bit
-        bitfield |= 1 << int_char;
-    }
-
     true
 }
 
